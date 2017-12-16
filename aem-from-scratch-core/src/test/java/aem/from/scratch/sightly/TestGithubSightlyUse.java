d37d425a50b3a1e13f39ef1b6b03e407205e92f3
Sampathkumar Rangadhamaiah <sampath@Sampathkumars-MacBook-Pro.local>
2017-12-16 12:41:26 +0530

    Change: 	TestGithubSightlyUse: merging test cases, using osgi context and mocks.

--- aem-from-scratch-core/src/test/java/aem/from/scratch/sightly/TestGithubSightlyUse.java
+++ aem-from-scratch-core/src/test/java/aem/from/scratch/sightly/TestGithubSightlyUse.java
@@ -2,11 +2,17 @@ package aem.from.scratch.sightly;
 
 import java.util.List;
 
+import org.apache.sling.testing.mock.osgi.junit.OsgiContext;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TestGithubSightlyUse {
+
+    @Rule
+    public final OsgiContext context = new OsgiContext();
 
     private GithubSightlyUse githubSightlyUseUnderTest;
 
@@ -16,13 +22,10 @@ public class TestGithubSightlyUse {
     }
 
     @Test
-    public void testGetRepositories() {
-        List<String> githubRepositories = githubSightlyUseUnderTest.getRepositories();
-        Assert.assertNotNull(githubRepositories);
-    }
-
-    @Test
     public void testGetRepositoriesContainsTenRepos() {
+        IGithubRepoFetcher repoFetcher = Mockito.mock(IGithubRepoFetcher.class);
+        context.registerService(IGithubRepoFetcher.class, repoFetcher);
+        context.registerInjectActivateService(githubSightlyUseUnderTest);
         List<String> githubRepositories = githubSightlyUseUnderTest.getRepositories();
         Assert.assertNotNull(githubRepositories);
         Assert.assertEquals(10, githubRepositories.size());

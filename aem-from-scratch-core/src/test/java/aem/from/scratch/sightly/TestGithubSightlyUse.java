0304bb40af442b1c9e66e6446f40990aaa438bba
Sampathkumar Rangadhamaiah <sampath@Sampathkumars-MacBook-Pro.local>
2017-12-16 08:25:03 +0530

    Adding setup method to test class.

--- aem-from-scratch-core/src/test/java/aem/from/scratch/sightly/TestGithubSightlyUse.java
+++ aem-from-scratch-core/src/test/java/aem/from/scratch/sightly/TestGithubSightlyUse.java
@@ -3,11 +3,17 @@ package aem.from.scratch.sightly;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestGithubSightlyUse {
 
     private GithubSightlyUse githubSightlyUseUnderTest;
+
+    @Before
+    public void setup() {
+        githubSightlyUseUnderTest = new GithubSightlyUse();
+    }
 
     @Test
     public void testGetRepositories() {

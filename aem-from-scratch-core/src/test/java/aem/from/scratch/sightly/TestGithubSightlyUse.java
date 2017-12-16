2f75990b6d7af952c75a311ae15aeb5e550006bf
Sampathkumar Rangadhamaiah <sampath@Sampathkumars-MacBook-Pro.local>
2017-12-16 07:50:07 +0530

    Adding dependencies for junit.

--- aem-from-scratch-core/src/test/java/aem/from/scratch/sightly/TestGithubSightlyUse.java
+++ aem-from-scratch-core/src/test/java/aem/from/scratch/sightly/TestGithubSightlyUse.java
@@ -1,5 +1,14 @@
 package aem.from.scratch.sightly;
 
+import org.junit.Test;
+
 public class TestGithubSightlyUse {
+
+    private GithubSightlyUse githubSightlyUseUnderTest;
+
+    @Test
+    public void testGetRepositories() {
+        githubSightlyUseUnderTest.getRepositories();
+    }
 
 }

41a35412d02a2dae3f0496bcd7b55418851d693e
Sampathkumar Rangadhamaiah <sampath@Sampathkumars-MacBook-Pro.local>
2017-12-16 08:26:42 +0530

    Update:
    	GithubSightlyUse: #getRepositories - returning empty arraylist.

--- aem-from-scratch-core/src/main/java/aem/from/scratch/sightly/GithubSightlyUse.java
+++ aem-from-scratch-core/src/main/java/aem/from/scratch/sightly/GithubSightlyUse.java
@@ -1,13 +1,14 @@
 package aem.from.scratch.sightly;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GithubSightlyUse {
 
     public List<String> getRepositories() {
+        List<String> githubRepositories = new ArrayList<>();
 
-        return null;
-
+        return githubRepositories;
     }
 
 }
